5157ead54457f76d685283c9ec4360b4ffec5911
Sergio Garnica <sgarnica1902@gmail.com>
2021-08-16 19:20:54 -0500

    Delete all custom js files

--- assets/js/bootstrap.js
+++
@@ -1,9 +0,0 @@
-const navItems = document.querySelector(".offcanvas-body").firstElementChild;
-
-window.addEventListener("load", () => {
-    if (this.document.body.offsetWidth > 990) {
-    navItems.classList.add('align-items-center')
-  } else {
-    navItems.classList.remove('align-items-center')
-  }
-})
